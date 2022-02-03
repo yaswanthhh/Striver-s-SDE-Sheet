0bd0d02190a40a07cb0ad7059b7435029060fe2c
Yaswanth Udayakumar <68239314+yaswanthhh@users.noreply.github.com>
2022-02-03 20:14:33 +0530

    Delete Week 2: Arrays Part-II directory

--- Week 2: Arrays Part-II/2.1_rotate_matrix.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def rotate(self, matrix):
-        for i in range(len(matrix)):
-            for j in range (i, len(matrix[0])):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in matrix:
-            i.reverse()

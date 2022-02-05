81358d7987dfecf7f9bd155dc8f207506ae0865e
Yaswanth Udayakumar <68239314+yaswanthhh@users.noreply.github.com>
2022-02-05 12:15:22 +0530

    Update 2.6_count_inversions.py

--- Solutions/Day 2: Arrays-Part II/2.6_count_inversions.py
+++ Solutions/Day 2: Arrays-Part II/2.6_count_inversions.py
@@ -1,7 +1,46 @@
-def getInversions(arr, n):
-	ctr = 0
-	for i in range(n-1):
-		for j in range(i+1, n):
-			if arr[i] > arr[j] and i < j:
-				ctr += 1
-	return ctr
+#Source: https://www.geeksforgeeks.org/counting-inversions/
+
+def mergeSort(arr, n):
+    temp_arr = [0]*n
+    return _mergeSort(arr, temp_arr, 0, n-1)
+ 
+ 
+def _mergeSort(arr, temp_arr, left, right): 
+    inv_count = 0 
+    if left < right: 
+        mid = (left + right)//2 
+        inv_count += _mergeSort(arr, temp_arr,
+                                left, mid)
+        inv_count += _mergeSort(arr, temp_arr,
+                                mid + 1, right)
+        inv_count += merge(arr, temp_arr, left, mid, right)
+    return inv_count
+ 
+ 
+def merge(arr, temp_arr, left, mid, right):
+    i = left     
+    j = mid + 1  
+    k = left     
+    inv_count = 0
+ 
+    while i <= mid and j <= right: 
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            temp_arr[k] = arr[j]
+            inv_count += (mid-i + 1)
+            k += 1
+            j += 1
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var] 
+    return inv_count

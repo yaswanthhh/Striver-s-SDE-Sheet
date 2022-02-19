c850084ec21d4f6def8aa722d5e1c34980cec16b
Yaswanth Udayakumar <68239314+yaswanthhh@users.noreply.github.com>
2022-02-19 21:26:03 +0530

    Create 7.2_copy_ll.py

---
+++ Solutions/Day 7: Linked List and Arrays/7.2_copy_ll.py
@@ -0,0 +1,20 @@
+class Solution:
+    def copyRandomList(self, head):
+        if head is None:
+            return None
+        new_node = {}
+        new_head = Node(head.val)
+        new_node[head] = new_head
+        p1 = head
+        p2 = new_head
+        while p1.next is not None:
+            p2.next = Node(p1.next.val)
+            new_node[p1.next] = p2.next
+            p2 = p2.next
+            p1 = p1.next
+        p1 = head
+        while p1 is not None:
+            if p1.random is not None:
+                new_node[p1].random = new_node[p1.random]
+            p1 = p1.next
+        return new_head

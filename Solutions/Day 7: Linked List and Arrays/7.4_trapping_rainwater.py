0e29089ab2911a83326fcde0b645208539015425
Yaswanth Udayakumar <68239314+yaswanthhh@users.noreply.github.com>
2022-02-19 22:50:01 +0530

    Create 7.4_trapping_rainwater.py

---
+++ Solutions/Day 7: Linked List and Arrays/7.4_trapping_rainwater.py
@@ -0,0 +1,19 @@
+class Solution:
+    def trap(self, height):
+        left, right = 0, len(height)-1
+        res = 0
+        maxleft, maxright = 0, 0
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] >= maxleft:
+                    maxleft = height[left]
+                else:
+                    res += maxleft - height[left]
+                left += 1
+            else:
+                if height[right] >= maxright:
+                    maxright = height[right]
+                else:
+                    res += maxright - height[right]
+                right -= 1
+        return res

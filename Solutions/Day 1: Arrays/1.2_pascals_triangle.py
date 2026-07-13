29a17ba5e5bc9148df76fcc83965509c8483008e
Yaswanth Udayakumar <68239314+yaswanthhh@users.noreply.github.com>
2026-07-14 00:12:09 +0200

    Refactor Pascal's Triangle generation method

--- Solutions/Day 1: Arrays/1.2_pascals_triangle.py
+++ Solutions/Day 1: Arrays/1.2_pascals_triangle.py
@@ -1,15 +1,11 @@
-class Solution(object):        
-    def generate(self, numRows):
-        pascals_triangle = []
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        list = []
         for i in range(numRows):
-            temp = []
-            for j in range(0, i+1):
-                if j > i-j:
-                    j = i-j
-                res = 1
-                for l in range(j):
-                    res *= (i-l)
-                    res //= l+1
-                temp.append(res)
-            pascals_triangle.append(temp)
-        return pascals_triangle
+            list.append([])
+            for j in range(i+1):
+                if j==0 or j==i:
+                    list[i].append(1)
+                else:
+                    list[i].append(list[i-1][j-1]+list[i-1][j])
+        return list
